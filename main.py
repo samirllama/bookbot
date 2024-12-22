68745a7478630e2f3e8d71164b0b99060ef6c5e7
Samir Lama <samirllama@gmail.com>
2024-12-22 11:28:31 -0600

    add report of books

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
+from curses.ascii import isalpha
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict =get_chars_dict(text)
+    chars_sorted = chars_dict_to_sorted_list(chars_dict)
+
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted:
+        if not item['char'].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for c in num_chars_dict:
+        sorted_list.append({"char": c, "num": num_chars_dict[c]})
+    sorted_list.sort( reverse=True, key=sort_on)
+    return sorted_list
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 main()
